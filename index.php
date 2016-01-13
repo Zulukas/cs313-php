389198a38e27ec5f61a69f354cd7272cbef2c276
Zulukas <1996nissan200sx@gmail.com>
2016-01-13 10:49:05 -0700

    Added a navbar!

--- index.php
+++ index.php
@@ -1,4 +1,37 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Kevin Andres - CS 313</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Assignments</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
+  <h3>Inverted Navbar</h3>
+  <p>An inverted navbar is black instead of gray.</p>
+</div>
+
+</body>
+</html>
+
+
+<!-- <!DOCTYPE html>
 <html>
 	<head>
 		<title>My first PHP page</title>
@@ -7,17 +40,8 @@
 		<div>
 			This is a pretty awesome page, bossman.
 
-<?php
-$name = "Br. Burton";
-echo "<br>This is coming from PHP!<br>";
-echo "Hello, $name<br>";
 
-for ($i = 0; $i < 10; $i++)
-{
-	print "<div>WHOA!!</div>\n";
-}
-?>
 
 		</div>
 	</body>
-</html>
+</html> -->
