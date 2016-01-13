cf38ae26e7aa5b0dddfafa03e5888b7147d04ccf
Zulukas <1996nissan200sx@gmail.com>
2016-01-13 11:06:43 -0700

    Added some text!

--- index.php
+++ index.php
@@ -11,20 +11,31 @@
 <body>
 
 <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Assignments</a></li>
-    </ul>
-  </div>
+	<div class="container-fluid">
+    	<div class="navbar-header">
+      		<a class="navbar-brand" href="#">Kevin Andres - CS 313</a>
+    	</div>
+		<ul class="nav navbar-nav">
+  			<li class="active"><a href="#">Home</a></li>
+  			<li><a href="#">Assignments</a></li>
+		</ul>
+	</div>
 </nav>
   
 <div class="container">
-  <h3>Inverted Navbar</h3>
-  <p>An inverted navbar is black instead of gray.</p>
+	<h3>Kevin Andres - CS 313 Homepage</h3>
+
+	<hr class="featurette-divider">
+
+    <div class="row featurette">
+  		<div class="col-md-7">
+  			<h2 class="featurette-heading">About Me</h2>
+  			<p class="lead">Wow, I'm a pretty awesome person!</p>
+		</div>
+		<div>
+			<img class="featurette-image img-fluid center-block" data-src"holder.js/500x500/auto" alt="Generic placeholder image">
+		</div>
+	</div>
 </div>
 
 </body>
