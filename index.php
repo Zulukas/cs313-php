38520f2a7d5c59ab4c56a00590dd51f9a42a3118
Zulukas <1996nissan200sx@gmail.com>
2016-01-13 11:08:50 -0700

    Added some text!

--- index.php
+++ index.php
@@ -28,14 +28,14 @@
 	<hr class="featurette-divider">
 
     <div class="row featurette">
-  		<div class="col-md-7">
-  			<h2 class="featurette-heading">About Me</h2>
-  			<p class="lead">Wow, I'm a pretty awesome person!</p>
-		</div>
-		<div>
-			<img class="featurette-image img-fluid center-block" data-src"holder.js/500x500/auto" alt="Generic placeholder image">
-		</div>
-	</div>
+        <div class="col-md-7 col-md-push-5">
+            <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+        <div class="col-md-5 col-md-pull-7">
+            <img class="featurette-image img-fluid center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        </div>
+    </div>
 </div>
 
 </body>
