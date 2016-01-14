655156ff3d0197ae05234645e20f64788a852fbb
Zulukas <1996nissan200sx@gmail.com>
2016-01-14 12:07:35 -0700

    Updated assignments.php to include first assignment

--- assignments.php
+++ assignments.php
@@ -1,10 +1,38 @@
 <!DOCTYPE html>
-
-<html>
+<html lang="en">
 <head>
-	<title>Assignments</title>
+  <title>Kevin Andres - CS 313</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
 <body>
+
+<nav class="navbar navbar-inverse">
+	<div class="container-fluid">
+    	<div class="navbar-header">
+      		<a class="navbar-brand" href="index.php">Kevin Andres - CS 313</a>
+    	</div>
+		<ul class="nav navbar-nav">
+  			<li class="active"><a href="index.php">Home</a></li>
+  			<li><a href="assignments.php">Assignments</a></li>
+		</ul>
+	</div>
+</nav>
+  
+<div class="container">
 	<h3>CS 313 Assignments</h3>
+
+	<hr class="featurette-divider">
+
+    <div class="row featurette">
+    	<ul>
+    		<li><a href="#">Survey<a></li>
+    	</ul>
+    </div>
+</div>
+
 </body>
 </html>
